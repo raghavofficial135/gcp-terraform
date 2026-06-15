d3ec2add854fcd812567260482807bee869531f5
Raghav <dwhraghav@gmail.com>
2026-06-15 11:50:10 -0500

    updated main.tfwith project variable

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name                        = "github-action-bucket"
+  project                     = "ipom-manufacturing"
   location                    = "US"
   force_destroy               = true
   uniform_bucket_level_access = true
